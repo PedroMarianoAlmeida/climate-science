29f53ddbebe89180e2aa3b9909f2169f386fef59
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2022-02-09 19:27:03 -0300

    add html structure

--- src/components/QuestionAnimated.jsx
+++ src/components/QuestionAnimated.jsx
@@ -1,3 +1,22 @@
-const QuestionAnimated = () => <h1>Question Animated</h1>;
+const QuestionAnimated = () => (
+  <div>
+    <h6>Which of the below statements about electricity is not true?</h6>
+
+    <div>
+      <input type="checkbox" />
+      <label>Electricity is measured in units called watts</label>
+    </div>
+
+    <div>
+      <input type="checkbox" />
+      <label>Electricity flows at the speed of light</label>
+    </div>
+
+    <div>
+      <input type="checkbox" />
+      <label>Electricity is a primary energy source</label>
+    </div>
+  </div>
+);
 
 export default QuestionAnimated;
