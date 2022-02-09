7579959f579b5efc1105a5728433ccaa1fff902d
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2022-02-09 19:38:28 -0300

    add conteiner figma styles

--- src/components/QuestionAnimated.jsx
+++ src/components/QuestionAnimated.jsx
@@ -1,22 +1,33 @@
 const QuestionAnimated = () => (
-  <div>
-    <h6>Which of the below statements about electricity is not true?</h6>
+  <>
+    <div id="container">
+      <h6>Which of the below statements about electricity is not true?</h6>
 
-    <div>
-      <input type="checkbox" />
-      <label>Electricity is measured in units called watts</label>
-    </div>
+      <div>
+        <input type="checkbox" />
+        <label>Electricity is measured in units called watts</label>
+      </div>
 
-    <div>
-      <input type="checkbox" />
-      <label>Electricity flows at the speed of light</label>
-    </div>
+      <div>
+        <input type="checkbox" />
+        <label>Electricity flows at the speed of light</label>
+      </div>
 
-    <div>
-      <input type="checkbox" />
-      <label>Electricity is a primary energy source</label>
+      <div>
+        <input type="checkbox" />
+        <label>Electricity is a primary energy source</label>
+      </div>
     </div>
-  </div>
+    <style jsx>{`
+      #container {
+        width: 500px;
+        height: 351px;
+        border: 0.5px solid #6231ec;
+        filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+        border-radius: 74px;
+      }
+    `}</style>
+  </>
 );
 
 export default QuestionAnimated;
